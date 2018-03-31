7647133ef2616d9b8a952f4a60a618755327cc52
KingBenx <kingbenx17@gmail.com>
2018-03-31 13:23:59 +0300

    errors.py refactor [Fixes #155327560]

--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,15 @@
-from flask import jsonify
+from flask import jsonify, request
 """These are custom messages for handling json replies """
 
 
 def make_json_reply(title, message):
     json_message = {title: message}
     return jsonify(json_message)
+
+def test_input(message,code):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = make_json_reply('error',message)
+        response.status_code = code 
+        return response
+    else:
+        return message,code
--- app/main/errors.py
+++ app/main/errors.py
@@ -1,22 +1,14 @@
 from . import main
 from flask import request, jsonify
-
+from ..functions import test_input
 
 @main.app_errorhandler(404)
 def page_not_known(e):
     """Error handler for unknown routes"""
-    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
-        response = jsonify({'error': 'not found'})
-        response.status_code = 404
-        return response
-    return 'Page Not found', 404
+    return test_input('Page not found',404)
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
     """Error handler for server errors"""
-    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
-        response = jsonify({'error': 'internal server error'})
-        response.status_code = 500
-        return response
-    return 'Internal Server Error', 500
+   return test_input('Internal Server Error',500)
